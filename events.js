bf92ac3f1105c5f58bd84e0aaeb128832d14042e
Samiker <168203446+Samiker69@users.noreply.github.com>
2024-12-02 19:38:11 +0300

    upload

--- events.js
+++ events.js
@@ -34,5 +34,178 @@ bot.on('message', async (msg) => {
         }
     }
 });
+
+//from VinTeRuS
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./tags.db');
+const cache = new Map(); // Cache for tags
+
+// Initialize the tags table in the database
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS tags (
+    id INTEGER PRIMARY KEY,
+    tag TEXT UNIQUE,
+    description TEXT
+  )`);
+});
+
+// Function to send messages to the chat or thread
+function sendMessage(chatId, text, options = {}) {
+  const messageOptions = options.message_thread_id ? { message_thread_id: options.message_thread_id } : {};
+  bot.sendMessage(chatId, text, messageOptions);
+}
+
+async function sendDelMessage(chatId, text, options = {}, msg) {
+    const messageThreadId = msg.message_thread_id;
+    const messageOptions = options.message_thread_id ? { message_thread_id: options.message_thread_id } : {};
+    const del = await bot.sendMessage(chatId, text, messageOptions);
+
+    setTimeout(async () => {
+        await bot.deleteMessage(del.chat.id, del.message_id, { message_thread_id: messageThreadId });
+    }, 3000);
+}
+
+// Function to check if the user is an admin in the chat
+async function isAdmin(chatId, userId) {
+  try {
+    const admins = await bot.getChatAdministrators(chatId);
+    return admins.some(admin => admin.user.id === userId);
+  } catch (error) {
+    console.error('Error checking admin:', error);
+    return false;
+  }
+}
+
+bot.onText(/\/addtag (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  const userId = msg.from.id;
+  const messageThreadId = msg.message_thread_id;
+  const [tagName, ...descriptionArr] = match[1].split(' ');
+  const tagDescription = descriptionArr.join(' ');
+
+  // Check if the user is an admin
+  if (!await isAdmin(chatId, userId)) {
+    await sendDelMessage(chatId, 'У вас нет разрешения на добавление тегов.', { message_thread_id: messageThreadId }, msg);
+    setTimeout(async () => {
+        await bot.deleteMessage(chatId, msg.message_id, { message_thread_id: messageThreadId });
+    }, 3000);
+    return;
+  }
+
+  // Insert the new tag into the database
+  db.run(`INSERT INTO tags (tag, description) VALUES (?, ?)`, [tagName, tagDescription], async function (err) {
+    if (err) {
+        await sendDelMessage(chatId, 'Ошибка: тег уже существует или произошла ошибка.', { message_thread_id: messageThreadId }, msg);
+        setTimeout(async () => {
+            await bot.deleteMessage(chatId, msg.message_id, { message_thread_id: messageThreadId });
+        }, 3000);
+    } else {
+      cache.set(tagName, tagDescription); // Cache the new tag
+      sendMessage(chatId, `Тег '${tagName}' был успешно добавлен.`, { message_thread_id: messageThreadId });
+    }
+  });
+}); 
+
+// Get the description of a tag
+bot.onText(/\/tag (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  const messageThreadId = msg.message_thread_id;
+  const tagName = match[1];
+
+  // If the tag is cached, return it directly
+  if (cache.has(tagName)) {
+    return sendMessage(chatId, `${cache.get(tagName)}`, { message_thread_id: messageThreadId });
+  }
+
+  // Retrieve the tag from the database
+  db.get(`SELECT description FROM tags WHERE tag = ?`, [tagName], async (err, row) => {
+    if (err, !row) {
+        await sendDelMessage(chatId, `Тег '${tagName}' не найден.`, { message_thread_id: messageThreadId }, msg);
+      setTimeout(async () => {
+        await bot.deleteMessage(chatId, msg.message_id, { message_thread_id: messageThreadId });
+    }, 3000);
+    } else {
+      cache.set(tagName, row.description); // Cache the tag for future use
+      sendMessage(chatId, `${row.description}`, { message_thread_id: messageThreadId });
+    }
+  });
+});
+
+// Delete a tag (only for admins)
+bot.onText(/\/deletetag (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  const userId = msg.from.id;
+  const messageThreadId = msg.message_thread_id;
+  const tagName = match[1];
+
+  // Check if the user is an admin
+  if (!await isAdmin(chatId, userId)) {
+    await sendDelMessage(chatId, 'У вас нет разрешения на удаление тегов.', { message_thread_id: messageThreadId }, msg);
+    setTimeout(async () => {
+        await bot.deleteMessage(chatId, msg.message_id, { message_thread_id: messageThreadId });
+    }, 3000);
+    return;
+  }
+
+  // Delete the tag from the database
+  db.run(`DELETE FROM tags WHERE tag = ?`, [tagName], async function (err) {
+    if (cache.changes === 0) {
+        await sendDelMessage(chatId, `Тег '${tagName}' не найден или не был удален.`, { message_thread_id: messageThreadId }, msg);
+      setTimeout(async () => {
+        await bot.deleteMessage(chatId, msg.message_id, { message_thread_id: messageThreadId });
+    }, 3000);
+    } else {
+      cache.delete(tagName); // Remove the tag from the cache
+      sendMessage(chatId, `Тег '${tagName}' был успешно удален.`, { message_thread_id: messageThreadId });
+    }
+  });
+});
+
+// List all tags
+bot.onText(/\/alltags/, (msg) => {
+  const chatId = msg.chat.id;
+  const messageThreadId = msg.message_thread_id;
+
+  db.all('SELECT tag FROM tags', [], (err, rows) => {
+  if (err || rows.length === 0) {
+    sendMessage(chatId, 'Нет доступных тегов.', { message_thread_id: messageThreadId });
+  } else {
+    const tagsList = rows.map(row => row.tag).join(', ');
+    sendMessage(chatId, `Доступные теги: ${tagsList}`, { message_thread_id: messageThreadId });
+  }
+});
+});
+
+bot.onText(/.tag (.+)/, async (msg, match) => {
+    const chatId = msg.chat.id;
+    const messageThreadId = msg.message_thread_id;
+    const tagName = match[1];
+  
+    // If the tag is cached, return it directly
+    if (cache.has(tagName)) {
+      return sendMessage(chatId, `${cache.get(tagName)}`, { message_thread_id: messageThreadId });
+    }
+  
+    // Retrieve the tag from the database
+    db.get(`SELECT description FROM tags WHERE tag = ?`, [tagName], async (err, row) => {
+      if (err, !row) {
+          await sendDelMessage(chatId, `Тег '${tagName}' не найден.`, { message_thread_id: messageThreadId }, msg);
+        setTimeout(async () => {
+          await bot.deleteMessage(chatId, msg.message_id, { message_thread_id: messageThreadId });
+      }, 3000);
+      } else {
+        cache.set(tagName, row.description); // Cache the tag for future use
+        sendMessage(chatId, `${row.description}`, { message_thread_id: messageThreadId });
+      }
+    });
+});
+
+// Cache cleanup (e.g., every 10 minutes)
+setInterval(() => {
+cache.clear();
+console.log('Tags cache has been cleared.');
+}, 10 * 60 * 1000); // Clear the cache every 10 minutes
+
 module.exports = bot;
 console.log(`event.js запущен!`);
